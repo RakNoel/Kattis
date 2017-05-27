8c7871b9f6c7242a95f32c9319126b2eeba3474e
Oskar Leonhard Fretheim Leirvåg <oskar.leirvag@student.uib.no>
2017-05-27 21:58:19 +0200

    so i guess this is the layout

--- kattis/src/Different.hs
+++ kattis/src/Different.hs
@@ -1,7 +1,15 @@
-solve [] = []
-solve (a:b:rest) = abs(a - b):(solve rest)
+module Main where
 
+solve :: [Integer] -> [Integer]
+solve xs =
+    if length xs == 0
+    then []
+    else (abs ((xs !! 0) - (xs !! 1))) : solve (drop 2 xs)
+
+--Input output
 readInput = (map read) . words
 writeOutput = unlines . (map show)
 
+--Reads in, solves, prints out
+main :: IO()
 main = interact (writeOutput . solve . readInput)
