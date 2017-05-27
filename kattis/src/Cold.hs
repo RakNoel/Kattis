db9ed62cdc5a58f5ffa15dfc9630d20ff62a42e2
Oskar Leonhard Fretheim Leirvåg <oskar.leirvag@student.uib.no>
2017-05-27 22:27:09 +0200

    Yeeeah!

---
+++ kattis/src/Cold.hs
@@ -0,0 +1,13 @@
+module Main where
+
+solve :: [Int] -> Int
+solve xs = length [x | x <- xs, x < 0]
+
+--Input output
+readInput = (map read) . words
+
+--Reads in, solves, prints out
+main :: IO()
+main = do
+    discard <- getLine
+    interact (show . solve . readInput)
