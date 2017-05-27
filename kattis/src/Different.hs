c134f79e7639d5b9ba4095991a162f897fa3688b
Oskar Leonhard Fretheim Leirvåg <oskar.leirvag@student.uib.no>
2017-05-27 20:52:59 +0200

    wont exit :(

--- kattis/src/Different.hs
+++ kattis/src/Different.hs
@@ -1,13 +1,16 @@
 module Main where
 import Prelude
+import Control.Monad
 
 someFunc :: (Num a) => a -> a -> a
 someFunc a b = abs (a - b)
 
+main :: IO()
 main = do
-    [a, b] <- fmap ((map read) . words) getLine
-    [c, d] <- fmap ((map read) . words) getLine
-    [e, f] <- fmap ((map read) . words) getLine
-    print (someFunc a b)
-    print (someFunc c d)
-    print (someFunc e f)
+    fLine <- getLine
+    if null fLine
+        then return()
+        else do
+            let readline = words fLine in
+                print (someFunc (read (head readline)) (read (last readline)))
+            main
