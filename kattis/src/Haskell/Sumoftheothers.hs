a4cc13732a2a4edbd4310abc17bbb41edde96859
Oskar Leonhard Fretheim Leirvåg <oskar.leirvag@student.uib.no>
2017-06-09 19:25:20 +0200

    Well well... IO nightmare

--- kattis/src/Haskell/Sumoftheothers.hs
+++ kattis/src/Haskell/Sumoftheothers.hs
@@ -1,17 +1,16 @@
 module Main where
+import Data.List
 
 solve :: [Int] -> Int
 solve xs = calculate xs (sum xs)
 
 calculate :: [Int] -> Int -> Int
-calculate [] _ = error "No solution"
 calculate (x:xs) tot
     | tot - x == x = x
-    | tot - x == 0 = x
     | otherwise = calculate xs tot
 
 readLine :: String -> [Int]
 readLine = (map read) . words
 
 main :: IO()
-main = interact $ show . solve . readLine
+main = interact (\x -> intercalate "\n" $ map (show . solve . readLine) (lines x))
