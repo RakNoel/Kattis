382077df08a6aededd82a5f43535d2928a63a372
Oskar Leirvåg <oskar.leir@gmail.com>
2019-02-18 22:36:10 +0100

    Small cleanup in repo

--- src/Java/CD/CD.java
+++ src/Java/CD/CD.java
@@ -1,9 +1,9 @@
 package Java.CD;
 
-import java.io.*;
-import java.util.StringTokenizer;
+import Java.kattio;
 
 //Created by RakNoel, 02.04.2017.
+
 public class CD {
     public static void main(String[] args) {
         kattio kattio = new kattio(System.in, System.out);
@@ -25,14 +25,13 @@ public class CD {
             int equalCD = 0;
             int lastKnown = 0;
 
-            for(int i = 0; i < jack; i++)
-                for (int j = lastKnown; j < jill; j++){
-                    if(jackList[i] == jillList[j]) {
+            for (int i = 0; i < jack; i++)
+                for (int j = lastKnown; j < jill; j++) {
+                    if (jackList[i] == jillList[j]) {
                         equalCD++;
-                        lastKnown = j+1;
+                        lastKnown = j + 1;
                         break;
-                    }
-                    else if (jackList[i] < jillList[j]) {
+                    } else if (jackList[i] < jillList[j]) {
                         lastKnown = j;
                         break;
                     }
@@ -41,62 +40,5 @@ public class CD {
 
             System.out.println(equalCD);
         }
-    }
-}
-
-class kattio extends PrintWriter {
-    private BufferedReader r;
-    private String line;
-    private StringTokenizer st;
-    private String token;
-
-    public kattio(InputStream i) {
-        super(new BufferedOutputStream(System.out));
-        r = new BufferedReader(new InputStreamReader(i));
-    }
-
-    public kattio(InputStream i, OutputStream o) {
-        super(new BufferedOutputStream(o));
-        r = new BufferedReader(new InputStreamReader(i));
-    }
-
-    public boolean hasMoreTokens() {
-        return peekToken() != null;
-    }
-
-    public int getInt() {
-        return Integer.parseInt(nextToken());
-    }
-
-    public double getDouble() {
-        return Double.parseDouble(nextToken());
-    }
-
-    public long getLong() {
-        return Long.parseLong(nextToken());
-    }
-
-    public String getWord() {
-        return nextToken();
-    }
-
-    private String peekToken() {
-        if (token == null)
-            try {
-                while (st == null || !st.hasMoreTokens()) {
-                    line = r.readLine();
-                    if (line == null) return null;
-                    st = new StringTokenizer(line);
-                }
-                token = st.nextToken();
-            } catch (IOException e) {
-            }
-        return token;
-    }
-
-    private String nextToken() {
-        String ans = peekToken();
-        token = null;
-        return ans;
     }
 }
